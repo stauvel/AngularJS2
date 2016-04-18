b63abae00fc193ac5fcd272b3356c2301a033b18
Seb <sebastien.tauvel@gmail.com>
2016-04-19 01:05:36 +0200

    Adding a first router

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,15 +1,26 @@
 import { Component }       from 'angular2/core';
 import { HeroService }     from './hero.service';
 import { HeroesComponent } from './heroes.component';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
+
+@RouteConfig([
+    {
+        path: '/heroes',
+        name: 'Heroes',
+        component: HeroesComponent
+    }
+])
 
 @Component({
     selector: 'my-app',
     template: `
-<h1>{{title}}</h1>
-<my-heroes></my-heroes>
+        <h1>{{title}}</h1>
+        <a [routerLink]="['Heroes']">Heroes</a>
+        <router-outlet></router-outlet>
 `,
-directives: [HeroesComponent],
-    providers: [
+directives: [ROUTER_DIRECTIVES],
+providers: [
+    ROUTER_PROVIDERS,
     HeroService
 ]
 })
